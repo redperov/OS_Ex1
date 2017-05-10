a89df5111ee40896a694446b43582d6d587ba614
redperov <redperov@gmail>
2017-05-10 19:18:55 +0300

    Divided work to functions

--- ex12.c
+++ ex12.c
@@ -9,6 +9,49 @@
 
 #define MAX_SIZE 160
 
+typedef struct {
+
+    //Compilation status.
+    int compileStatus;
+
+    //Execution status.
+    int executeStatus;
+
+    //Comparison status.
+    int compareStatus;
+} Status;
+
+typedef struct {
+
+    //Student's directory path.
+    char *dirPath;
+
+    //Student's C file path.
+    char *cFilePath;
+
+    //Student's executable file path.
+    char *execFilePath;
+
+    //TODO delete if not needed
+    //Student's path to the main folder.
+    char *mainDirPath;
+
+    //The path to the main directory of students.
+    char *homePath;
+
+    //Student's dirent.
+    struct dirent *dirent;
+
+    //Depth to c file.
+    int depth;
+
+    //Boolean does the student have multiple directories.
+    int isMultipleDirectories;
+
+    Status status;
+
+} Student;
+
 /**
  * Writes a message to the given file.
  * @param file file descriptor.
@@ -26,13 +69,11 @@ void readFromFile(int fileDesc, char buffer[]);
 /**
  * Searches for a C file in the given directory.
  * @param initPath initial path to search.
- * @param studentDirent student dirent to the path.
- * @param isMultDirectories checks if there are multiple directories.
+ * @param student student struct.
  * @return path if found, else NULL.
  */
 char *
-findCFile(char *initPath, struct dirent *studentDirent, int *isMultDirectories,
-          int *depth);
+findCFile(char *initPath, Student *student);
 
 /**
  * Checks if the given path belongs to a directory.
@@ -40,6 +81,51 @@ findCFile(char *initPath, struct dirent *studentDirent, int *isMultDirectories,
  * @return boolean.
  */
 int isDirectory(char *path);
+
+/**
+ * Adds error message to a student in the results file.
+ * @param fileDesc file descriptor.
+ * @param errorMessage error message.
+ */
+void addErrorToStudent(int fileDesc, char *errorMessage);
+
+/**
+ * Initializes all the student's members.
+ * @return Student.
+ */
+Student initStudent();
+
+/**
+ * Waits for the child to finish execution.
+ * @param status.
+ * @return -1 exec failed, 0 program failed, 1 succeeded.
+ */
+int waitForChildExec(int *status);
+
+/**
+ * Compiles the student's C file.
+ * @param student *Student
+ * @return 0 if failed, 1 if succeeded.
+ */
+int compileStudentFile(Student *student);
+
+/**
+ * Executes the student's C file.
+ * @param student student.
+ * @param inputFilePath input file path.
+ * @return 0 if failed, 1 if succeeded.
+ */
+int executeStudentFile(Student *student, char *inputFilePath);
+
+/**
+ * Compares between the correct and student's outputs.
+ * @param student student
+ * @param correctOutput correct output path.
+ * @param studentOutput student's output path.
+ * @return -1 different, 0 similar, 1 same.
+ */
+int compareStudentFile(Student *student, char *correctOutput,
+                       char *studentOutput);
 
 int main(int argc, char *argv[]) {
 
@@ -88,6 +174,7 @@ int main(int argc, char *argv[]) {
         exit(1);
     }
 
+    //Creates the results file.
     results = open("results.csv", O_CREAT | O_WRONLY,
                    0777);
 
@@ -99,229 +186,100 @@ int main(int argc, char *argv[]) {
             continue;
         }
 
-        int isMultDirectories = 0;
-        int depth             = 0;
-        studentPath = findCFile(dirPath, studentDirent, &isMultDirectories,
-                                &depth);
+        Student student;
 
-        if (studentPath == 0) {
+        student.dirent   = studentDirent;
+        student.homePath = dirPath;
 
-            if (isMultDirectories) {
+        writeToFile(results, student.dirent->d_name);
+
+        //Search for the student's C file.
+        studentPath = findCFile(dirPath, &student);
+
+        student.cFilePath = studentPath;
+
+        //Check if C file was found.
+        if (student.cFilePath == 0) {
+
+            //Check if the reason is multiple directories.
+            if (student.isMultipleDirectories) {
 
                 //TODO grade missing
                 //TODO is \n necessary when writing to .csv
-                char message[MAX_SIZE];
-                snprintf(message, MAX_SIZE, "%s,MULTIPLE_DIRECTORIES\n",
-                         studentDirent->d_name);
-
-                writeToFile(results, message);
+                writeToFile(results, ",MULTIPLE_DIRECTORIES");
 
                 printf("Multiple directories.\n");
+
             } else {
 
-                char message[MAX_SIZE];
-                snprintf(message, MAX_SIZE, "%s,NO_C_FILE\n",
-                         studentDirent->d_name);
-
-                writeToFile(results, message);
+                writeToFile(results, ",NO_C_FILE");
             }
 
-            printf("No c: %s\n", studentDirent->d_name);
+            printf("No c: %s\n", student.dirent->d_name);
 
 
         } else {
-            printf("Found: %s depth: %d\n", studentPath, depth);
 
-            pid_t compilePId;
-            pid_t execPId;
-            int   compileStatus;
-            int   execStatus;
-            int   isDirChanged;
+            printf("Found: %s depth: %d\n", student.cFilePath, student.depth);
 
-            compilePId = fork();
+            int compileResult;
+            int executeResult;
+            int compareResult;
+            int chDirResult;
 
-            if (compilePId < 0) {
+            //compiles the C file.
+            compileResult = compileStudentFile(&student);
 
-                perror("Error: fork failed.\n");
-                exit(1);
+            if (compileResult == 0) {
+
+                writeToFile(results, ",COMPILATION_ERROR");
+                //TODO make sure the main moves to the next student.
+                continue;
             }
 
-            if (compilePId == 0) {
+            executeResult = executeStudentFile(&student, inputPath);
 
-                //Move to student directory.
-                strcpy(studentDir, dirPath);
-                strcat(studentDir, "/");
-                strcat(studentDir, studentDirent->d_name);
+            if (executeResult == 0) {
 
-                //TODO go one dir up at the end.
-                isDirChanged = chdir(studentDir);
+                //TODO check what to do in that case, maybe just perror and exit
+            }
 
-                //Check if changing directory succeeded.
-                if (isDirChanged < 0) {
+            compareResult = compareStudentFile(&student, outputPath,
+                                               "studentOutput.txt");
 
-                    perror("Error: failed to change directory.\n");
-                    exit(1);
-                }
-                //TODO change student.out to a.out
-                char *args[] = {"gcc", studentPath, "-o", "student.out", 0};
-                int  retExec;
+            switch (compareResult) {
+                case -1:
+                    writeToFile(results, "BAD_OUTPUT");
+                    break;
+                case 0:
+                    writeToFile(results, "SIMILAR_OUTPUT");
+                    break;
+                case 1:
+                    writeToFile(results, "WRONG_DIRECTORY");
+                    break;
 
-                printf("Enter child process.\n");
-                retExec = execvp("gcc", args);
+                default:
+                    break;
+            }
 
-                if(retExec == -1){
+            //Return back to main directory.
+            chDirResult = chdir(mainPath);
 
-                    perror("Error: execution failed.\n");
-                    exit(1);
-                }
+            if (chDirResult == -1) {
 
-            } else {
-
-                //TODO should compile all files and then execute and use fork to save time?
-
-                wait(&compileStatus);
-
-                if (WIFEXITED(compileStatus)) {
-
-                    printf("finished compileStatus:%d.\n",
-                           WEXITSTATUS(compileStatus));
-
-                    //Check if compilation succeeded.
-                    if (WEXITSTATUS(compileStatus) == 1) {
-
-                        char message[MAX_SIZE];
-                        snprintf(message, MAX_SIZE, "%s,COMPILATION_ERROR\n",
-                                 studentDirent->d_name);
-
-                        writeToFile(results, message);
-
-                    } else {
-
-                        execPId = fork();
-
-                        if (execPId < 0) {
-
-                            perror("Error: fork failed.\n");
-                            exit(1);
-                        }
-
-                        if (execPId == 0) {
-
-                            //TODO change student.out to a.out
-                            char *argsStudent[] = {"./student.out", inputPath, 0};
-                            int studentOutputFile;
-                            int inputFile;
-                            int execVal;
-
-                            studentOutputFile = open("studentOutput.txt", O_CREAT | O_WRONLY, 777);
-
-                            if(studentOutputFile < 0){
-
-                                perror("Error: failed to open file.\n");
-                                exit(1);
-                            }
-
-                            inputFile = open(inputPath, O_RDONLY);
-
-                            if(inputFile < 0){
-
-                                perror("Error: failed to open file.\n");
-                                exit(1);
-                            }
-
-                            //Redirect input and output to files.
-                            dup2(inputFile, 0);
-                            dup2(studentOutputFile, 1);
-
-                            close(studentOutputFile);
-                            close(inputFile);
-
-                            execVal = execvp("./student.out", argsStudent);
-
-                            if(execVal == -1){
-
-                                perror("Error: execution failed.\n");
-                                exit(1);
-                            }
-                        }
-                        else{
-
-                            wait(&execStatus);
-
-                            if(WIFEXITED(execStatus)){
-
-                                printf("finished execStatus:%d.\n",
-                                       WEXITSTATUS(execStatus));
-
-                                if(WEXITSTATUS(execStatus) == 1){
-
-                                    //TODO should there be a handle to a runtime error?
-                                }
-                                else{
-
-                                    pid_t compPId;
-                                    int compStatus;
-
-                                    compPId = fork();
-
-                                    if(compPId == 0){
-
-                                    }
-                                    else{
-
-                                        wait(&compStatus);
-
-                                        if(WIFEXITED(compStatus)){
-
-                                            printf("finished execStatus:%d.\n",
-                                                   WEXITSTATUS(execStatus));
-
-                                            if(WEXITSTATUS(compStatus) == 1){
-
-                                                perror("Error: comparison failed.\n");
-                                                exit(1);
-                                            }
-                                    }
-
-                                    char *argsComp[] = {"./comp.out", "studentOutput.txt", outputPath, 0};
-                                    int compExec;
-
-                                    compExec = execvp("./comp.out", argsComp);
-
-                                    if(compExec == -1){
-
-                                        perror("Error: execution failed.\n");
-                                        exit(1);
-                                    }
-                                }
-                            }
-                            else{
-
-                                //TODO should you write error and even exit in that case
-                                perror("Execution failed.\n");
-                                exit(1);
-                            }
-                        }
-
-
-                    }
-
-                } else {
-                    //TODO should you write error and even exit in that case
-                    perror("Execution failed.\n");
-                    exit(1);
-                }
+                perror("Error: chdir failed");
+                exit(1);
             }
         }
 
+        writeToFile(results, "\n");
     }
 
-    //TODO check if all opened files were closed.
+//TODO check if all opened files were closed.
     close(results);
 }
 
-char *findCFile(char *initPath, struct dirent *studentDirent,
-                int *isMultDirectories, int *depth) {
+char *findCFile(char *initPath, Student *student) {
 
     int  stop       = 0;
     int  dirCounter = 0;
@@ -330,7 +288,7 @@ char *findCFile(char *initPath, struct dirent *studentDirent,
     DIR  *dir;
 
     strcpy(finalPath, initPath);
-    strcpy(nextFile, studentDirent->d_name);
+    strcpy(nextFile, student->dirent->d_name);
 
     while (!stop) {
 
@@ -343,22 +301,25 @@ char *findCFile(char *initPath, struct dirent *studentDirent,
         if (!isDirectory(finalPath)) {
 
             //Return final path.
-            char *retPath = (char *) malloc(strlen(finalPath) * sizeof(char));
+            //TODO free
+            char *retPath = (char *) malloc(
+                    strlen(finalPath) * sizeof(char));
+
             strcpy(retPath, finalPath);
 
             return retPath;
         }
 
         dir = opendir(finalPath);
-        *depth += 1;
+        student->depth += 1;
 
         dirCounter = 0;
 
         //Checks the amount of folders the student has is legal.
-        while ((studentDirent = readdir(dir)) != 0) {
+        while ((student->dirent = readdir(dir)) != 0) {
 
-            if (strcmp(studentDirent->d_name, ".") == 0 ||
-                strcmp(studentDirent->d_name, "..") == 0) {
+            if (strcmp(student->dirent->d_name, ".") == 0 ||
+                strcmp(student->dirent->d_name, "..") == 0) {
                 continue;
             }
 
@@ -367,14 +328,14 @@ char *findCFile(char *initPath, struct dirent *studentDirent,
             //Stop searching if more that on inner folder exists.
             if (dirCounter > 1) {
 
-                *isMultDirectories = 1;
+                student->isMultipleDirectories = 1;
                 closedir(dir);
                 return 0;
             }
 
             //TODO does using strcpy add \0 at the end? if not then add with strcat
             //Copy next name.
-            strcpy(nextFile, studentDirent->d_name);
+            strcpy(nextFile, student->dirent->d_name);
         }
 
         //Check if the path contained another file.
@@ -442,4 +403,194 @@ void readFromFile(int fileDesc, char buffer[]) {
     }
 
 
+}
+
+int compileStudentFile(Student *student) {
+
+    pid_t compilePId;
+    pid_t execPId;
+    int   compileStatus;
+    int   execStatus;
+    int   isDirChanged;
+
+    compilePId = fork();
+
+    if (compilePId < 0) {
+
+        perror("Error: fork failed.\n");
+        exit(1);
+    }
+
+    if (compilePId == 0) {
+
+        //Move to student directory.
+//        strcpy(student->mainDirPath,);
+//        strcat(studentDir, "/");
+//        strcat(studentDir, studentDirent->d_name);
+
+        char studentMainDirPath[MAX_SIZE];
+        snprintf(studentMainDirPath, MAX_SIZE, "%s/%s",
+                 student->homePath, student->dirent->d_name);
+
+        //TODO go one dir up at the end.
+        isDirChanged = chdir(studentMainDirPath);
+
+        //Check if changing directory succeeded.
+        if (isDirChanged < 0) {
+
+            perror("Error: failed to change directory.\n");
+            exit(1);
+        }
+        //TODO change student.out to a.out
+        char *args[] = {"gcc", student->cFilePath, "-o", "student.out", 0};
+        int  retExec;
+
+        printf("Enter child process.\n");
+        retExec = execvp("gcc", args);
+
+        if (retExec == -1) {
+
+            perror("Error: execution failed.\n");
+            exit(1);
+        }
+    } else {
+
+        //TODO should compile all files and then execute and use fork to save time?
+
+        return waitForChildExec(&student->status.compileStatus);
+    }
+}
+
+int executeStudentFile(Student *student, char *inputFilePath) {
+    pid_t execPId;
+    execPId = fork();
+
+    //Check if fork succeeded.
+    if (execPId < 0) {
+
+        perror("Error: fork failed.\n");
+        exit(1);
+    }
+
+    if (execPId == 0) {
+
+        //TODO change student.out to a.out
+        char *argsStudent[] = {"./student.out", inputFilePath,
+                               0};
+        int  studentOutputFile;
+        int  inputFile;
+        int  execVal;
+
+        //TODO this file already gets opened in ex11, make sure it doesn't cause problems.
+        //TODO delete all student files after they are no longer needed
+        studentOutputFile = open("studentOutput.txt",
+                                 O_CREAT | O_WRONLY, 777);
+
+        //Check if studentOutputFile was opened.
+        if (studentOutputFile < 0) {
+
+            perror("Error: failed to open file.\n");
+            exit(1);
+        }
+
+        inputFile = open(inputFilePath, O_RDONLY);
+
+        //Check if inputFile was opened.
+        if (inputFile < 0) {
+
+            perror("Error: failed to open file.\n");
+            exit(1);
+        }
+
+        //Redirect input and output to files.
+        dup2(inputFile, 0);
+        dup2(studentOutputFile, 1);
+
+        close(studentOutputFile);
+        close(inputFile);
+
+        execVal = execvp("./student.out", argsStudent);
+
+        //Check if execvp failed.
+        if (execVal == -1) {
+
+            perror("Error: execution failed.\n");
+            exit(1);
+        }
+    } else {
+
+        //Wait for execution to end.
+        return waitForChildExec(&student->status.executeStatus);
+    }
+}
+
+int compareStudentFile(Student *student, char *correctOutput,
+                       char *studentOutput) {
+
+    pid_t compPId;
+
+    compPId = fork();
+
+    if (compPId == 0) {
+
+        char *argsComp[] = {"./comp.out", correctOutput, studentOutput,
+                            0};
+        int  compExec;
+
+        compExec = execvp("./comp.out",
+                          argsComp);
+
+        if (compExec == -1) {
+
+            perror("Error: execution failed.\n");
+            exit(1);
+        }
+
+    } else {
+
+        return waitForChildExec(&student->status.compareStatus);
+    }
+
+}
+
+Student initStudent() {
+
+    Student student;
+
+    memset(student.dirPath, 0, MAX_SIZE);
+    memset(student.cFilePath, 0, MAX_SIZE);
+    memset(student.execFilePath, 0, MAX_SIZE);
+    memset(student.mainDirPath, 0, MAX_SIZE);
+
+    student.dirent                = 0;
+    student.depth                 = -1;
+    student.isMultipleDirectories = 0;
+
+    student.status.compileStatus = 0;
+    student.status.compareStatus = 0;
+    student.status.executeStatus = 0;
+
+    return student;
+}
+
+int waitForChildExec(int *status) {
+
+    wait(status);
+
+    if (WIFEXITED(*status)) {
+
+        printf("finished exec:%d.\n",
+               WEXITSTATUS(*status));
+
+        //Check if execution succeeded.
+        if (WEXITSTATUS(*status) == 1) {
+
+            return 0;
+        }
+
+        return 1;
+    }
+
+    //TODO delete if not necessary
+//        return -1;
 }
